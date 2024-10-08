cf6afbee55ea02878c772663baa2cf0df8e6b6c3
ToPrakhar <toprakhargupta@gmail.com>
2024-10-07 22:46:22 -0700

    Create UserController.java

---
+++ UserController.java
@@ -0,0 +1,18 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @QueryMapping
+    public List<User> getUsersByFilters(@Argument List<UserFilterInput> filters) {
+        return userService.getUsersByFilters(filters);
+    }
+}
